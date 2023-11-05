f8964bb1e2177181a0dc9c11de58ab0a1f548200
Neamul Khan <neamulkhan@yahoo.com>
2023-11-04 20:04:17 -0400

    pageFactory included

--- src/test/java/com/project5HomeLocator/HomePageLocator.java
+++ src/test/java/com/project5HomeLocator/HomePageLocator.java
@@ -1,10 +1,14 @@
 package com.project5HomeLocator;
 
+import org.openqa.selenium.support.PageFactory;
+
 import utility.BaseClass_project5;
 
 public class HomePageLocator extends BaseClass_project5{
 	
 	public HomePageLocator () {
+		
+		PageFactory.initElements(driver, this);
 		
 		
 	}
